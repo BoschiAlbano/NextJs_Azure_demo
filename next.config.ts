edd9b3993b3bfc7fce18ac95beec02ebd6679f54
BoschiAlbano <iprogramacion17@gmail.com>
2024-12-17 18:35:02 -0300

    Deploy Standalone

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
     output: "standalone",
+    reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
     },
 };
 
